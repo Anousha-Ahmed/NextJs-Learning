19062e561baf524638a003d743ad62aa978fbbcc
Anousha Ahmed <anoushaashfaque@gmail.com>
2026-07-22 23:40:07 +0500

    Add styling in navbar

--- my-app/app/components/Navbar.js
+++ my-app/app/components/Navbar.js
@@ -4,20 +4,40 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [message, setMessage] = useState("Welcome!");
+  const [message, setMessage] = useState("");
 
   return (
     <>
-      <nav style={{ background: "#333", padding: "15px" }}>
-        <Link href="/" style={{ marginRight: "20px", color: "white" }}>Home</Link>
-        <Link href="/about" style={{ marginRight: "20px", color: "white" }}>About</Link>
-        <Link href="/blog" style={{ color: "white" }}>Blog</Link>
-      </nav>
+      <nav style={{ background: "#333", padding: "15px", color: "white" }}>
+        <Link href="/" style={{ marginRight: "20px", color: "white" }}>
+          Home
+        </Link>
 
-      <div style={{ padding: "10px" }}>
-        <button onClick={() => setMessage("You clicked the button!")}>Click Me</button>
-        <p>{message}</p>
-      </div>
+        <Link href="/about" style={{ marginRight: "20px", color: "white" }}>
+          About
+        </Link>
+
+        <Link href="/blog" style={{ color: "white" }}>
+          Blog
+        </Link>
+
+        <div
+          style={{
+            width: "120px",
+            border: "1px solid white",
+            padding: "10px",
+            display: "inline-block",
+            marginLeft: "1050px",
+            verticalAlign: "middle",
+          }}
+        >
+          <button onClick={() => setMessage("Welcome")}>
+            Click Me
+          </button>
+
+          <p>{message}</p>
+        </div>
+      </nav>
     </>
   );
 }
